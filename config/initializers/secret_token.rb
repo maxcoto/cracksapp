7339faf54ba5eaef0eec580883e38df8fdd7d314
Maximiliano Perez Coto <maxi@assembla.com>
2014-05-08 20:27:02 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cracksapp::Application.config.secret_key_base = '604fd56b7703090719b9bf8b8c9500eb187a19c41665c95e1f0256eb79f778a1e8162d90c5372ddb3266ba318e8085ee8af0bd8235162bca9ed5cd69e5c52ee9'
