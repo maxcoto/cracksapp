8aab390fa4efdc47e84b550dce6d53279b3dd4f0
Maximiliano Perez Coto <maxi@assembla.com>
2014-05-22 21:57:45 -0300

    wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Cracksapp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'cracksapp.io' }
+  config.action_mailer.default_url_options = { host: 'cracksapp.herokuapp.com' }
 end
